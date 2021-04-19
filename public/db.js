f795bd5daf943978d2df73ec4e9e51f752aefe44
Arash Saeedi <saeediarash84@gmail.com>
2021-04-19 19:49:51 -0400

    found similarities with this and activity 22 in week 17

--- public/db.js
+++ public/db.js
@@ -5,3 +5,39 @@
 
 // also need a manifest.webmanifest alond with a service-worker.js this will allow for the files to 
 // be cached and kept offline when need be
+let db;
+// create a new db request for a "budget" database
+const request = indexedDB.open("budget",1);
+
+request.onupgradeneeded = function (e) {
+    // create object store called "pending" and set autoIncrement to true
+    const db = e.target.result;
+    db.createObjectStore("pending", {autoIncrement: true});
+};
+
+request.onsucess = function (e) {
+    const db = e.target.result;
+    // checking if the app is online before starting to read from db
+    if (navigator.onLine) {
+        checkDB();
+    }
+};
+
+request.onerror = function (e) {
+    console.log("ERROR!!! " + e.target.errorCode);
+};
+
+function saveRecord(record) {
+    // create a transaction on the opending db with a readwrite access
+    const transaction = db.transaction(["pending"], "readwrite");
+
+    // access your pending object store
+    const store = transaction.createObjectStore("pending");
+
+    // add the record to your store with add method
+    store.add(record);
+};
+
+function checkDB() {
+
+}
