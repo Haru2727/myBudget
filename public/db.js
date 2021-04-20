03bdf1c4ed876136a867b78102b178286687c7aa
Arash Saeedi <saeediarash84@gmail.com>
2021-04-20 13:31:59 -0400

    cleaned up the comments in the db file

--- public/db.js
+++ public/db.js
@@ -2,11 +2,6 @@
 // have commands for onupgradeneeded, onsuccess, onerror
 // then need to save the record and and check the database for it once
 // it is reopened.  If it isnt there it should add or create it to the DB
-
-const { response } = require("express");
-
-// also need a manifest.webmanifest alond with a service-worker.js this will allow for the files to 
-// be cached and kept offline when need be
 let db;
 // create a new db request for a "budget" database
 const request = indexedDB.open("budget",1);
